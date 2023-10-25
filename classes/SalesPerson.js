6ca9b1ccfecf9d41f014b014c8acec3fd8dc6ff7
ljenchik <pelagenkolena@gmail.com>
2023-10-25 11:18:10 +0100

    Added two more classes

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,19 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
-
-
-    
+class SalesPerson extends Employees {
+    constructor(name, position, salary, clients) {
+        super(name, position, salary);
+        this.clients = clients;
+        this.totalSales = 0;
+    }
+    getSalesNumber() {
+        return this.totalSales;
+    }
+    makeSale(amount) {
+        this.totalSales += amount;
+    }
 }
 
 module.exports = {
     SalesPerson,
-}
+};
--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,10 +1,18 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
-
-
-    
+class SoftwareEngineer extends Employees {
+    #programmingLanguages = ["JavaScript", "Java", "Python"];
+    constructor(name, position, salary) {
+        super(name, position, salary);
+    }
+    getProgrammingLanguages() {
+        return this.#programmingLanguages;
+    }
+    setProgrammingLanguages(language) {
+        this.#programmingLanguages.push(language);
+    }
 }
 
 module.exports = {
     SoftwareEngineer,
-}
+};
